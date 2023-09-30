7888b7844c874e5a16e64d1361d10f862325ccfa
aaryadav <aaryaxb1@gmail.com>
2023-09-30 20:02:24 +0530

    add link to personal site in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
 import HeartIcon from "@/public/heart.svg"
+import Link from "next/link"
 
 const Footer = () => {
     return (
         <div className="footer">
-            Made with &nbsp;<Image src={HeartIcon} alt={'<3'} />&nbsp; by Aaryaman.
+            Made with &nbsp;<Image src={HeartIcon} alt={'<3'} />&nbsp; by &nbsp;<u><Link href={"https://aaryadav.github.io/"}>Aaryaman</Link></u>.
         </div>
     )
 }
