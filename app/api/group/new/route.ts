d9116bccb6adddc9ddf423fcd905cc3ef593075d
Aaryaman Yadav <aaryaxb1@gmail.com>
2023-10-09 20:55:39 +0530

    fix: generate invite code for group

--- app/api/group/new/route.ts
+++ app/api/group/new/route.ts
@@ -5,9 +5,11 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient();
 
-const generateInviteCode = () => {
-    const inviteCode = "pixelbuds";
-    return inviteCode;
+const generateInviteCode = (name) => {
+    const inviteCodeBase = name.split(' ').join('').toLowerCase();
+    const randomNumber = Math.floor(Math.random() * 9000) + 1000;
+
+    return `${inviteCodeBase}-${randomNumber}`;
 }
 
 export async function POST(request: Request) {
@@ -15,7 +17,7 @@ export async function POST(request: Request) {
     const { name, adminId } = await request.json()
 
     try {
-        const inviteCode = generateInviteCode();
+        const inviteCode = generateInviteCode(name);
 
         // Create a new group
         const newGroup = await prisma.group.create({
